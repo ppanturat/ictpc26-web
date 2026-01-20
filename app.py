585a203f37f98654f680a05b4079816d18b3029b
ppanturat <punnaporn2548@gmail.com>
2026-01-20 13:07:49 +0700

    update rules page

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/rules')
+def rules():
+    return render_template('rules.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
